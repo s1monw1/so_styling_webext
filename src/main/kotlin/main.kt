ce78dac08a387a69b14da4d6bafcc710e6d15970
Simon <simon.wirtzq@gmail.com>
2017-11-24 00:29:55 +0100

    ext created with KotlinJJS

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,37 @@
+import org.w3c.dom.*
+import kotlin.browser.document
+
+fun main(args: Array<String>) {
+    val querySelectorAll = document.querySelectorAll("[id^=answer-]")
+    querySelectorAll.forEach {
+        with(it as HTMLDivElement) {
+            style.backgroundColor = "#eff0f1"
+            (getElementsByTagName("a")[0] as HTMLAnchorElement).greyOut()
+            (getElementsByClassName("excerpt")[0] as HTMLDivElement).greyOut()
+        }
+    }
+
+    val unanswered = document.getElementsByClassName("question-summary")
+    unanswered.forEach {
+        val element = it as HTMLDivElement
+        if (element.querySelector(".unanswered") != null) {
+            element.style.backgroundColor = "#FFFFE0"
+        }
+    }
+}
+
+fun HTMLElement.greyOut() {
+    style.color = "#8f9090"
+}
+
+fun NodeList.forEach(act: (Node?) -> Unit) {
+    (0 until length).forEach { act(this[it]) }
+}
+
+fun HTMLCollection.forEach(act: (Element?) -> Unit) {
+    (0 until length).forEach { act(this[it]) }
+}
+
+
+
+
