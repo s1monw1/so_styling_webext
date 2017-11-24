0bffdc074e45e0c8abc3ddf20944c5dfd9e017e0
SIM <simon.wirtz@n-design.de>
2017-11-24 09:07:37 +0100

    code refac, new version

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,34 +2,30 @@ import org.w3c.dom.*
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    val querySelectorAll = document.querySelectorAll("[id^=answer-]")
-    querySelectorAll.forEach {
-        with(it as HTMLDivElement) {
-            style.backgroundColor = "#eff0f1"
-            (getElementsByTagName("a")[0] as HTMLAnchorElement).greyOut()
-            (getElementsByClassName("excerpt")[0] as HTMLDivElement).greyOut()
+    document.querySelectorAll("[id^=answer-]").forEach<HTMLDivElement> {
+        style.backgroundColor = "#eff0f1"
+        (getElementsByTagName("a")[0] as HTMLAnchorElement).greyOut()
+        (getElementsByClassName("excerpt")[0] as HTMLDivElement).greyOut()
+    }
+
+    document.getElementsByClassName("question-summary").forEach<HTMLDivElement> {
+        if (querySelector(".unanswered") != null) {
+            style.backgroundColor = "#FFFFE0"
         }
     }
 
-    val unanswered = document.getElementsByClassName("question-summary")
-    unanswered.forEach {
-        val element = it as HTMLDivElement
-        if (element.querySelector(".unanswered") != null) {
-            element.style.backgroundColor = "#FFFFE0"
-        }
-    }
 }
 
 fun HTMLElement.greyOut() {
     style.color = "#8f9090"
 }
 
-fun NodeList.forEach(act: (Node?) -> Unit) {
-    (0 until length).forEach { act(this[it]) }
+inline fun <reified T> NodeList.forEach(act: T.() -> Unit) {
+    (0 until length).forEach { (this[it] as T).act() }
 }
 
-fun HTMLCollection.forEach(act: (Element?) -> Unit) {
-    (0 until length).forEach { act(this[it]) }
+inline fun <reified T> HTMLCollection.forEach(act: T.() -> Unit) {
+    (0 until length).forEach { (this[it] as T).act() }
 }
 
 
